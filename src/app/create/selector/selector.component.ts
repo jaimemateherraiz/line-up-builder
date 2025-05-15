d561789edae589119deb1ae2b941806be6383fe9
Jose Fernández <jf.developr@gmail.com>
2025-05-15 19:06:55 +0200

    Creacion de rutas, ajuste del navbar, componente create, estructuracion, componente selector

---
+++ src/app/create/selector/selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector',
+  imports: [],
+  templateUrl: './selector.component.html',
+  styleUrl: './selector.component.css'
+})
+export class SelectorComponent {
+
+}
